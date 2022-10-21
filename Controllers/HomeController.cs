477abe0b6b81b420c7900ff9c8b1c0b9b088a176
Raul Cesar Mulerschat <raulzito737@gmail.com>
2022-10-21 20:06:54 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Calculadora.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Calculadora.Controllers {
+    public class HomeController : Controller {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger) {
+            _logger = logger;
+        }
+
+        public IActionResult Index() {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Calcu calc) {
+            int a, b;
+            a = calc.value1;
+            b = calc.value2;
+            if (calc.Acao == "somar") {
+            calc.resultado = a + b;
+            }else if (calc.Acao == "subtrair") {
+                calc.resultado = a - b;
+            } else if (calc.Acao == "dividir") {
+                calc.resultado = a / b;
+            } else if (calc.Acao == "multiplicar") {
+                calc.resultado = a * b;
+            } else if (calc.Acao == "Limpar") {
+                calc.resultado = 0;
+                calc.value1 = 0;
+                calc.value2=0;
+            }
+
+            ViewData["resultado"]= calc.resultado;
+                return View();
+        }
+        public IActionResult Privacy() {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Calcu.cs
@@ -0,0 +1,10 @@
+﻿namespace Calculadora.Models {
+    public class Calcu {
+
+        public int value1 { get; set; }
+        public int value2 { get; set; }
+        public int resultado { get; set; }
+        public string Acao { get; set; }
+
+    }
+}
